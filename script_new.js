2db2a257094569cd06acdb107a1cf5aa9ae53c57
usfit <73701754+usfit@users.noreply.github.com>
2022-09-16 10:23:11 +0300

    Add files via upload

---
+++ script_new.js
@@ -0,0 +1,118 @@
+// Поле ввода
+let input = document.querySelector('.input-text');
+
+// Вывод результатов поиска
+let ul = document.querySelector('.search-list');
+// ul.classList.toggle('hide');
+
+// Вывод выбранных репо
+let ulAnswer = document.querySelector('.answer-list');
+
+// Создание словаря
+let repoAnswer = {};
+
+// Пустые значения в словаре
+function nullAnswer() {
+    for (let i = 0; i < 5; i++) {
+        repoAnswer[i] = {};
+    }
+    return repoAnswer;
+}
+
+// Получение репозиториев через API
+async function getRepositories(qInput) {
+    let url = `https://api.github.com/search/repositories?q=${qInput}&page=1&per_page=5&sort=stars&order=desc}`;
+    let repos = await fetch(url);
+    let answer = await repos.json();
+    let repositories = answer.items;
+    return repositories;
+}
+
+// Добавляение репозиториев в словарь
+function createAnswer(repositories) {
+    repositories.forEach((elem, id) => {
+        repoAnswer[id]['name'] = elem.name;
+        repoAnswer[id]['owner'] = elem.owner.login;
+        repoAnswer[id]['stars'] = elem['stargazers_count'];
+    })
+    return repoAnswer;
+}
+
+// Функция очистки
+
+function clearInfo() {
+    input.value = '';
+    ul.childNodes.forEach(child => {
+        child.textContent = '';
+    })
+    ul.classList.toggle('hide');
+}
+
+// Получение информации из репозиториев Основная функция
+async function getRepositoriesInfo() {
+    let repositories = await getRepositories(qInput);
+    repoAnswer = nullAnswer()
+    repoAnswer = createAnswer(repositories);
+
+    // Добавляем информацию в список на страницу
+    Object.keys(repoAnswer).forEach(key => {
+        ul.children[key].textContent = repoAnswer[key]['name'];
+    })
+
+    if (ul.classList.contains('hide')) {
+        ul.classList.toggle('hide');
+    }
+
+    return repoAnswer;
+}
+
+// Схлопывание запросов
+const debounce = (fn, debounceTime) => {
+    let time;
+    return function (...args) {
+        clearTimeout(time);
+        time = setTimeout(() => {
+            fn.apply(this, args);
+        }, debounceTime);
+    };
+};
+const debouncedFn = debounce(getRepositoriesInfo, 500);
+
+// Cлучатель событий в инпут
+input.addEventListener('input', () => {
+    qInput = input.value;
+    debouncedFn();
+})
+
+// Слушатель клика на список с делегированием
+ul.addEventListener('click', (event) => {
+    let target = event.target;
+    if (target.classList.value === 'search-list__element' && target.textContent !== '') {
+        let liAnswer = document.createElement('li');
+        liAnswer.classList.add('answer-list__element');
+        // Блок значка удаления
+        let close = document.createElement('div');
+        close.classList.add('close');
+        Object.keys(repoAnswer[target.id]).forEach(key => {
+            // Текстовое содержимое
+            let p = document.createElement('p');
+            p.textContent = `${key[0].toUpperCase() + key.slice(1) }: ${repoAnswer[target.id][key]}`;
+            liAnswer.append(p);
+            liAnswer.append(close);
+        })
+
+        ulAnswer.append(liAnswer);
+
+        // Чистим данные после клиека
+        clearInfo();
+    }
+})
+
+// Удаление элемента
+
+ulAnswer.addEventListener('click', (event) => {
+    let target = event.target;
+    if (target.classList.value == 'close') {
+        target.parentNode.remove();
+    }
+})
